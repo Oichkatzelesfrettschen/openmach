5bb78f8c75d04c52e23241913686b193b4ac7d6b
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-06-09 01:36:57 -0700

    Add Doxygen comments

--- libmach/lmm/lmm_add.c
+++ libmach/lmm/lmm_add.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (c) 1995 The University of Utah and
  * the Computer Systems Laboratory at the University of Utah (CSL).
  * All rights reserved.
@@ -25,40 +25,55 @@
 
 #include <mach/machine/debug_reg.h>
 
-void lmm_add(lmm_t *lmm, vm_offset_t min, vm_size_t size,
-	     lmm_flags_t flags, lmm_pri_t pri)
-{
-	vm_offset_t max = min + size;
-	struct lmm_region *reg, **rp, *r;
+/**
+ * @brief Add a memory region to the low memory manager.
+ *
+ * This function inserts a new region into the region list sorted by
+ * priority and size.
+ *
+ * @param lmm   Low memory manager instance to add the region to.
+ * @param min   Starting address of the region.
+ * @param size  Size of the region in bytes.
+ * @param flags Attributes associated with the region.
+ * @param pri   Priority of the region.
+ *
+ * @return None.
+ */
+void lmm_add(lmm_t *lmm, vm_offset_t min, vm_size_t size, lmm_flags_t flags,
+             lmm_pri_t pri) {
+  vm_offset_t max = min + size;
+  struct lmm_region *reg, **rp, *r;
 
-	assert((sizeof(struct lmm_region) & ALIGN_MASK) == 0);
+  assert((sizeof(struct lmm_region) & ALIGN_MASK) == 0);
 
-	/* Align the start and end addresses appropriately.  */
-	min = (min + ALIGN_MASK) & ~ALIGN_MASK;
-	max &= ~ALIGN_MASK;
+  /* Align the start and end addresses appropriately.  */
+  min = (min + ALIGN_MASK) & ~ALIGN_MASK;
+  max &= ~ALIGN_MASK;
 
-	/* If there's not enough memory to do anything with,
-	   then just drop it on the floor.  */
-	if ((max < min) || (max - min < sizeof(struct lmm_region) + sizeof(struct lmm_node)))
-		return;
+  /* If there's not enough memory to do anything with,
+     then just drop it on the floor.  */
+  if ((max < min) ||
+      (max - min < sizeof(struct lmm_region) + sizeof(struct lmm_node)))
+    return;
 
-	/* Initialize the new region header and its initial free node.  */
-	reg = (struct lmm_region*)min;
-	reg->nodes = (struct lmm_node*)(reg + 1);
-	reg->nodes->next = 0;
-	reg->nodes->size = max - (vm_offset_t)reg->nodes;
-	reg->flags = flags;
-	reg->pri = pri;
-	reg->size = max - min;
-	reg->free = reg->nodes->size;
+  /* Initialize the new region header and its initial free node.  */
+  reg = (struct lmm_region *)min;
+  reg->nodes = (struct lmm_node *)(reg + 1);
+  reg->nodes->next = 0;
+  reg->nodes->size = max - (vm_offset_t)reg->nodes;
+  reg->flags = flags;
+  reg->pri = pri;
+  reg->size = max - min;
+  reg->free = reg->nodes->size;
 
-	/* Add the region to the lmm's region list in descending priority order.
-	   For regions with the same priority, sort from largest to smallest
-	   to reduce the average amount of list traversing we need to do.  */
-	for (rp = &lmm->regions;
-	     (r = *rp) && ((r->pri > pri) || ((r->pri == pri) && (r->size > reg->size)));
-	     rp = &r->next);
-	reg->next = r;
-	*rp = reg;
+  /* Add the region to the lmm's region list in descending priority order.
+     For regions with the same priority, sort from largest to smallest
+     to reduce the average amount of list traversing we need to do.  */
+  for (rp = &lmm->regions;
+       (r = *rp) &&
+       ((r->pri > pri) || ((r->pri == pri) && (r->size > reg->size)));
+       rp = &r->next)
+    ;
+  reg->next = r;
+  *rp = reg;
 }
-
